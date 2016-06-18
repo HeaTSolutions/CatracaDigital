ac933bf2f041ec20a8e5ba0d73cef0703b47bc79
Henrique Nogueira <hrqnogueira@gmail.com>
2016-06-17 23:47:54 -0300

    Added redirect from index to landing

--- CatracaDigital/urls.py
+++ CatracaDigital/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
+
+    # Handling root URL
+    url(r'^$', RedirectView.as_view(pattern_name='landing:index', permanent=False), name='index'),
+
     url(r'^admin/', admin.site.urls),
     url(r'core/', include('CatracaDigital.core.urls', namespace='core')),
     url(r'landing/', include('CatracaDigital.landing.urls', namespace='landing'))
